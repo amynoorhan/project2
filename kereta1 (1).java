210f6c4d90fa69f6b44648c482bba83d56d860d8
amynoorhan <32628263+amynoorhan@users.noreply.github.com>
2018-01-18 10:12:31 +0800

    Add files via upload

---
+++ kereta1 (1).java
@@ -0,0 +1,17 @@
+class Kereta1
+{
+   public static void main(String args[])
+
+   {
+
+   System.out.println ("BIL\t JENIS KERETA\n");
+
+   System.out.println ("1\t Proton Persona\n");
+
+   System.out.println ("2\t Honda City\n");
+   
+   System.out.println ("3\t Perodua Myvi\n");
+
+   }
+
+}
